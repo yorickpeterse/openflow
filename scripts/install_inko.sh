3cdb09c94756251c11b47a2ae47b234d08e55871
Yorick Peterse <git@yorickpeterse.com>
2023-02-12 04:00:10 +0100

    Initial commit

---
+++ scripts/install_inko.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+apk add --update make libffi libffi-dev rust cargo build-base git libgcc
+
+if [[ ! -d inko ]]
+then
+    git clone https://gitlab.com/inko-lang/inko.git inko
+fi
+
+cd inko
+cargo build --release --features libffi-system
+cd ..
